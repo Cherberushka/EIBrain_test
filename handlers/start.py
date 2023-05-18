c73c14c6d282be0db8f6b68d43434e20aa756f62
Cherberushka <kluev.ekb@mail.ru>
2023-05-18 22:16:29 +0500

    test buttons

--- handlers/start.py
+++ handlers/start.py
@@ -8,7 +8,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     kb = InlineKeyboardMarkup(row_width=1,
-                              inline_keyboard=[[InlineKeyboardButton(text="Начать чат с ИИ", callback_data="start")]])
+                              inline_keyboard=[[InlineKeyboardButton(text="Привет, я создан чтобы дать тебе ответы на интересующие тебя вопросы.", callback_data="start")]])
     await message.answer(
         f"Привет, {message.from_user.full_name}!",
         reply_markup=kb)
